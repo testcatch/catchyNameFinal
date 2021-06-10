b3369b59168c15082cc164d26ee8517ac0cc5412
Thomas Gkountas <thomas4uh@gmail.com>
2021-06-10 13:03:58 +0300

    small changes

--- src/main/java/tiles/Tile.java
+++ src/main/java/tiles/Tile.java
@@ -8,8 +8,6 @@ import graphics.Sprite;
 public class Tile {
 	
 	
-	// a tile will have a sprite 
-	//even when an error will occur a sprite will be displayed
 	public int x,y;
 	public Sprite sprite;
 	private int colorId;
@@ -17,20 +15,13 @@ public class Tile {
 	
 	private static Tile spawnGrassTile = new SpawnGrassTile(Sprite.spawnGrass,0xff00ff21);
 	private static Tile spawnWaterTile = new SpawnWaterTile(Sprite.spawnWater, 0xff0026ff);
-	//public static Tile spawnWood = new SpawnWoodTile(Sprite.spawnWood);
 	private static Tile spawnBricks1 = new SpawnWallTile(Sprite.spawnBricks1,0xff404040);
 	private static Tile spawnBricks2 = new SpawnWallTile(Sprite.spawnBricks2,0xffffd800);
 	private static Tile spawnBlood = new SpawnBloodTile(Sprite.spawnBlood,0xffff0000);
 	private static Tile spawnVoid = new SpawnVoidTile(Sprite.spawnVoid,0xff000000);
 	
 	
-	// colors used in Spawn image for representing Spawn Level Tiles
-//	public final static int colSpawnGrass = 0xff00ff21;
-//	public final static int colSpawnBlood = 0xffff0000;
-//	public final static int colSpawnBricks1 = 0xff404040;
-//	public final static int colSpawnBricks2 = 0xffffd800; 
-//	public final static int colSpawnWater = 0xff0026ff;
-//	public final static int colSpawnLocation = 0xff00ffff;
+
 	
 	
 	public Tile(Sprite sprite,int colorId) {
